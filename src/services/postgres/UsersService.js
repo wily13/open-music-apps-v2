f8d4810bf5829409811957eb99d967e107813c4b
wily13 <wily.355@gmail>
2021-06-24 10:29:35 +0700

    user authentication init

--- src/services/postgres/UsersService.js
+++ src/services/postgres/UsersService.js
@@ -2,6 +2,8 @@ const { nanoid } = require('nanoid');
 const { Pool } = require('pg');
 const bcrypt = require('bcrypt');
 const InvariantError = require('../../exceptions/InvariantError');
+const NotFoundError = require('../../exceptions/NotFoundError');
+const AuthenticationError = require('../../exceptions/AuthenticationError');
 
 class UsersService {
     constructor() {
@@ -38,8 +40,44 @@ class UsersService {
         const result = await this._pool.query(query);
 
         if (result.rows.length > 0) {
-            throw new InvariantError('Gagal menambahkan users. Username sudah digunakan.');
+            throw new InvariantError('Gagal menambahkan user. Username sudah digunakan.');
         }
+    }
+
+    async getUserById(userId) {
+        const query = {
+            text: 'SELECT id, username, fullname FROM users WHERE id = $1',
+            values: [userId],
+        };
+
+        const result = await this._pool.query(query);
+
+        if (!result.rows.length) {
+            throw new NotFoundError('User tidak ditemukan');
+        }
+        return result.rows[0];
+    }
+
+    async verifyUserCredential(username, password) {
+        const query = {
+            text: 'SELECT id, password FROM users WHERE username = $1',
+            values: [username],
+        };
+
+        const result = await this._pool.query(query);
+
+        if (!result.rows.length) {
+            throw new AuthenticationError('Kredensial yang Anda berikan salah');
+        }
+
+        const { id, password: hashedPassword } = result.rows[0];
+
+        const match = await bcrypt.compare(password, hashedPassword);
+
+        if (!match) {
+            throw new AuthenticationError('Kredensial yang Anda berikan salah');
+        }
+        return id;
     }
 }
 
